6967ec3bdf9decb227fed7ada2b25be9bba690f0
jeremiah@james-enterprise.com <coloradobowhunter@live.com>
2020-03-16 15:58:41 -0600

    removed pseudocode from bottom of js file

--- index.js
+++ index.js
@@ -15,27 +15,3 @@ $(function(){
      })   
 
      });
-
-
-
-// need to append entered value to .shopping list 
-// <li>
-// <span class="shopping-item">apples</span>
-// <div class="shopping-item-controls">
-//   <button class="shopping-item-toggle">
-//     <span class="button-label">check</span>
-//   </button>
-//   <button class="shopping-item-delete">
-//     <span class="button-label">delete</span>
-//   </button>
-// </div>
-// </li>
-
-// need to be able to switch classes between shopping-item-checked and null
-// in shopping-item span
-
-// need to be able to delete list item
-
-//     -must use this
-//     -need to be able to add using return 
-//     -must use cdn hosted jquery
